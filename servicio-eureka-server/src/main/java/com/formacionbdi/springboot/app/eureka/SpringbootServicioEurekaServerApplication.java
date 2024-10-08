ebfa176e459f98bfce97f3d6901773b8f873dae6
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-10-08 19:58:40 -0400

    1-INIT

--- servicio-eureka-server/src/main/java/com/formacionbdi/springboot/app/eureka/SpringbootServicioEurekaServerApplication.java
+++ servicio-eureka-server/src/main/java/com/formacionbdi/springboot/app/eureka/SpringbootServicioEurekaServerApplication.java
@@ -1,15 +1,22 @@
 package com.formacionbdi.springboot.app.eureka;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class SpringbootServicioEurekaServerApplication {
+public class SpringbootServicioEurekaServerApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootServicioEurekaServerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("**************************************");
+		System.out.println("EUREKA V-241008 19:55");
 	}
 
 }
