b58679e78792c8f0f4ed874497b5aabf703fce02
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-12-24 13:22:22 -0500

    V1.1.4

--- servicio-eureka-server/src/main/java/com/formacionbdi/springboot/app/eureka/SpringbootServicioEurekaServerApplication.java
+++ servicio-eureka-server/src/main/java/com/formacionbdi/springboot/app/eureka/SpringbootServicioEurekaServerApplication.java
@@ -16,7 +16,7 @@ public class SpringbootServicioEurekaServerApplication implements CommandLineRun
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("**************************************");
-		System.out.println("EUREKA V1.1.2 24-11-06 08:07");
+		System.out.println("EUREKA V1.1.4 24-12-24 13:21");
 	}
 
 }
